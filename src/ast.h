4f3593deb57a8c8cb2ded5c41e75fe22e8a45256
Nick Chapman <nic@amadido.co.uk>
2023-11-22 11:59:37 +0000

    indent code in namespace

--- src/ast.h
+++ src/ast.h
@@ -10,146 +10,144 @@ namespace llvm {
   class Value;
 }
 
-// TODO: use "ast" namespace?
-
-typedef std::string Name;
+typedef std::string Name; // TODO: move into ast namespace
 
 namespace ast {
 
-class Env; // eval.cpp
+  class Env; // eval.cpp
+  class TopCon; // codegen.cpp
+  class DefCon; // codegen.cpp
 
-class TopCon; // codegen.cpp
-class DefCon; // codegen.cpp
+  class Exp {
+  public:
+    virtual ~Exp() = default;
+    virtual int eval(Env&) = 0; // eval.cpp
+    virtual std::string pp() = 0; // pp-ast.cpp
+    virtual llvm::Value* codegen(DefCon&) = 0; // codegen.cpp
+  };
 
-class Exp {
-public:
-  virtual ~Exp() = default;
-  virtual int eval(Env&) = 0; // eval.cpp
-  virtual std::string pp() = 0; // pp-ast.cpp
-  virtual llvm::Value* codegen(DefCon&) = 0; // codegen.cpp
-};
+  class Var : public Exp {
+  private:
+    Name VarName;
+  public:
+    Var (::Name VarName) : VarName(VarName) {}
+    int eval(Env&) override;
+    std::string pp() override;
+    llvm::Value* codegen(DefCon&) override;
+  };
 
-class Var : public Exp {
-private:
-  Name VarName;
-public:
-  Var (::Name VarName) : VarName(VarName) {}
-  int eval(Env&) override;
-  std::string pp() override;
-  llvm::Value* codegen(DefCon&) override;
-};
+  class Num : public Exp {
+  private:
+    int NumValue;
+  public:
+    Num (int NumValue) : NumValue(NumValue) {}
+    int eval(Env&) override;
+    std::string pp() override;
+    llvm::Value* codegen(DefCon&) override;
+  };
 
-class Num : public Exp {
-private:
-  int NumValue;
-public:
-  Num (int NumValue) : NumValue(NumValue) {}
-  int eval(Env&) override;
-  std::string pp() override;
-  llvm::Value* codegen(DefCon&) override;
-};
+  class Mul : public Exp {
+  private:
+    up<Exp> MulLeft;
+    up<Exp> MulRight;
+  public:
+    Mul (up<Exp> MulLeft, up<Exp> MulRight)
+      : MulLeft(mv(MulLeft)), MulRight(mv(MulRight))
+    {}
+    int eval(Env&) override;
+    std::string pp() override;
+    llvm::Value* codegen(DefCon&) override;
+  };
 
-class Mul : public Exp {
-private:
-  up<Exp> MulLeft;
-  up<Exp> MulRight;
-public:
-  Mul (up<Exp> MulLeft, up<Exp> MulRight)
-    : MulLeft(mv(MulLeft)), MulRight(mv(MulRight))
-  {}
-  int eval(Env&) override;
-  std::string pp() override;
-  llvm::Value* codegen(DefCon&) override;
-};
+  class Add : public Exp {
+  private:
+    up<Exp> AddLeft;
+    up<Exp> AddRight;
+  public:
+    Add (up<Exp> AddLeft, up<Exp> AddRight)
+      : AddLeft(mv(AddLeft)), AddRight(mv(AddRight))
+    {}
+    int eval(Env&) override;
+    std::string pp() override;
+    llvm::Value* codegen(DefCon&) override;
+  };
 
-class Add : public Exp {
-private:
-  up<Exp> AddLeft;
-  up<Exp> AddRight;
-public:
-  Add (up<Exp> AddLeft, up<Exp> AddRight)
-    : AddLeft(mv(AddLeft)), AddRight(mv(AddRight))
-  {}
-  int eval(Env&) override;
-  std::string pp() override;
-  llvm::Value* codegen(DefCon&) override;
-};
+  class Sub : public Exp {
+  private:
+    up<Exp> SubLeft;
+    up<Exp> SubRight;
+  public:
+    Sub (up<Exp> SubLeft, up<Exp> SubRight)
+      : SubLeft(mv(SubLeft)), SubRight(mv(SubRight))
+    {}
+    int eval(Env&) override;
+    std::string pp() override;
+    llvm::Value* codegen(DefCon&) override;
+  };
 
-class Sub : public Exp {
-private:
-  up<Exp> SubLeft;
-  up<Exp> SubRight;
-public:
-  Sub (up<Exp> SubLeft, up<Exp> SubRight)
-    : SubLeft(mv(SubLeft)), SubRight(mv(SubRight))
-  {}
-  int eval(Env&) override;
-  std::string pp() override;
-  llvm::Value* codegen(DefCon&) override;
-};
+  class LessThan : public Exp {
+  private:
+    up<Exp> LessThanLeft;
+    up<Exp> LessThanRight;
+  public:
+    LessThan (up<Exp> LessThanLeft, up<Exp> LessThanRight)
+      : LessThanLeft(mv(LessThanLeft)), LessThanRight(mv(LessThanRight))
+    {}
+    int eval(Env&) override;
+    std::string pp() override;
+    llvm::Value* codegen(DefCon&) override;
+  };
 
-class LessThan : public Exp {
-private:
-  up<Exp> LessThanLeft;
-  up<Exp> LessThanRight;
-public:
-  LessThan (up<Exp> LessThanLeft, up<Exp> LessThanRight)
-    : LessThanLeft(mv(LessThanLeft)), LessThanRight(mv(LessThanRight))
-  {}
-  int eval(Env&) override;
-  std::string pp() override;
-  llvm::Value* codegen(DefCon&) override;
-};
+  class Ite : public Exp {
+  private:
+    up<Exp> IteCond;
+    up<Exp> IteThen;
+    up<Exp> IteElse;
+  public:
+    Ite (up<Exp> IteCond, up<Exp> IteThen, up<Exp> IteElse)
+      : IteCond(mv(IteCond))
+      , IteThen(mv(IteThen))
+      , IteElse(mv(IteElse))
+    {}
+    int eval(Env&) override;
+    std::string pp() override;
+    llvm::Value* codegen(DefCon&) override;
+  };
 
-class Ite : public Exp {
-private:
-  up<Exp> IteCond;
-  up<Exp> IteThen;
-  up<Exp> IteElse;
-public:
-  Ite (up<Exp> IteCond, up<Exp> IteThen, up<Exp> IteElse)
-    : IteCond(mv(IteCond))
-    , IteThen(mv(IteThen))
-    , IteElse(mv(IteElse))
-  {}
-  int eval(Env&) override;
-  std::string pp() override;
-  llvm::Value* codegen(DefCon&) override;
-};
+  class Call : public Exp {
+    Name CallFunc;
+    std::vector<up<Exp>> CallArgs;
+  public:
+    Call(Name CallFunc, std::vector<up<Exp>> CallArgs)
+      : CallFunc(CallFunc), CallArgs(mv(CallArgs)) {}
+    int eval(Env&) override;
+    std::string pp() override;
+    llvm::Value* codegen(DefCon&) override;
+  };
 
-class Call : public Exp {
-  Name CallFunc;
-  std::vector<up<Exp>> CallArgs;
-public:
-  Call(Name CallFunc, std::vector<up<Exp>> CallArgs)
-    : CallFunc(CallFunc), CallArgs(mv(CallArgs)) {}
-  int eval(Env&) override;
-  std::string pp() override;
-  llvm::Value* codegen(DefCon&) override;
-};
+  class Def {
+  public:
+    Name DefName;
+  private:
+    std::vector<Name> DefFormals;
+    up<Exp> DefBody;
+  public:
+    Def(Name DefName, std::vector<Name> DefFormals, up<Exp> DefBody)
+      : DefName(DefName), DefFormals(DefFormals), DefBody(mv(DefBody)) {}
+    std::string pp();
+    int apply(Env&,std::vector<int>);
+    void declare(TopCon&);
+    void codegen(TopCon&);
+  };
 
-class Def {
-public:
-  Name DefName;
-private:
-  std::vector<Name> DefFormals;
-  up<Exp> DefBody;
-public:
-  Def(Name DefName, std::vector<Name> DefFormals, up<Exp> DefBody)
-    : DefName(DefName), DefFormals(DefFormals), DefBody(mv(DefBody)) {}
-  std::string pp();
-  int apply(Env&,std::vector<int>);
-  void declare(TopCon&);
-  void codegen(TopCon&);
-};
-
-class Prog {
-public:
-  std::vector<up<Def>> ProgDefs;
-  Prog(std::vector<up<Def>> ProgDefs) : ProgDefs(mv(ProgDefs)) {}
-  std::string pp();
-  int eval();
-};
+  class Prog {
+  public:
+    std::vector<up<Def>> ProgDefs;
+    Prog(std::vector<up<Def>> ProgDefs) : ProgDefs(mv(ProgDefs)) {}
+    std::string pp();
+    int eval();
+  };
 
 }
+
 #endif // AST_H
--- src/pp-ast.cpp
+++ src/pp-ast.cpp
@@ -5,86 +5,86 @@
 
 namespace ast {
 
-std::string Var::pp() {
-  return VarName;
-}
-
-std::string Num::pp() {
-  return std::to_string(NumValue);
-}
-
-std::string Mul::pp() {
-  return "(" + MulLeft->pp() + " * " + MulRight->pp() + ")";
-}
-
-std::string Add::pp() {
-  return "(" + AddLeft->pp() + " + " + AddRight->pp() + ")";
-}
-
-std::string Sub::pp() {
-  return "(" + SubLeft->pp() + " - " + SubRight->pp() + ")";
-}
-
-std::string LessThan::pp() {
-  return "(" + LessThanLeft->pp() + " < " + LessThanRight->pp() + ")";
-}
-
-std::string Ite::pp() {
-  return "(" + IteCond->pp() + " ? " + IteThen->pp() + " : " + IteElse->pp() + ")";
-}
-
-std::string ppExps(std::vector<up<Exp>>& xs) {
-  std::string acc = "";
-  bool first = true;
-  for (auto &x : xs) {
-    if (!first) {
-      acc = acc + ",";
-    }
-    acc = acc + x->pp();
-    first = false;
+  std::string Var::pp() {
+    return VarName;
   }
-  return acc;
-}
 
-std::string Call::pp() {
-  return CallFunc + "(" + ppExps(CallArgs) + ")";
-}
-
-std::string ppNames(std::vector<Name> xs) {
-  std::string acc = "";
-  bool first = true;
-
-  for (auto &x : xs) {
-    if (!first) {
-      acc = acc + ",";
-    }
-    acc = acc + x;
-    first = false;
+  std::string Num::pp() {
+    return std::to_string(NumValue);
   }
-  return acc;
-}
 
-std::string Def::pp() {
-  return
-    "def " + DefName + "(" + ppNames(DefFormals) + "):\n  " +
-    DefBody->pp() + ";";
-}
-
-std::string ppDefs(std::vector<up<Def>>& xs) {
-  std::string acc = "";
-  bool first = true;
-  for (auto &x : xs) {
-    if (!first) {
-      acc = acc + "\n\n";
-    }
-    acc = acc + x->pp();
-    first = false;
+  std::string Mul::pp() {
+    return "(" + MulLeft->pp() + " * " + MulRight->pp() + ")";
   }
-  return acc;
-}
 
-std::string Prog::pp() {
-  return ppDefs(ProgDefs);
-}
+  std::string Add::pp() {
+    return "(" + AddLeft->pp() + " + " + AddRight->pp() + ")";
+  }
+
+  std::string Sub::pp() {
+    return "(" + SubLeft->pp() + " - " + SubRight->pp() + ")";
+  }
+
+  std::string LessThan::pp() {
+    return "(" + LessThanLeft->pp() + " < " + LessThanRight->pp() + ")";
+  }
+
+  std::string Ite::pp() {
+    return "(" + IteCond->pp() + " ? " + IteThen->pp() + " : " + IteElse->pp() + ")";
+  }
+
+  std::string ppExps(std::vector<up<Exp>>& xs) {
+    std::string acc = "";
+    bool first = true;
+    for (auto &x : xs) {
+      if (!first) {
+        acc = acc + ",";
+      }
+      acc = acc + x->pp();
+      first = false;
+    }
+    return acc;
+  }
+
+  std::string Call::pp() {
+    return CallFunc + "(" + ppExps(CallArgs) + ")";
+  }
+
+  std::string ppNames(std::vector<Name> xs) {
+    std::string acc = "";
+    bool first = true;
+
+    for (auto &x : xs) {
+      if (!first) {
+        acc = acc + ",";
+      }
+      acc = acc + x;
+      first = false;
+    }
+    return acc;
+  }
+
+  std::string Def::pp() {
+    return
+      "def " + DefName + "(" + ppNames(DefFormals) + "):\n  " +
+      DefBody->pp() + ";";
+  }
+
+  std::string ppDefs(std::vector<up<Def>>& xs) {
+    std::string acc = "";
+    bool first = true;
+    for (auto &x : xs) {
+      if (!first) {
+        acc = acc + "\n\n";
+      }
+      acc = acc + x->pp();
+      first = false;
+    }
+    return acc;
+  }
+
+  std::string Prog::pp() {
+    return ppDefs(ProgDefs);
+  }
 
 }
